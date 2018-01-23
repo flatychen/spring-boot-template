c2181b5d7f50fb6b0d03b05db400abe9e07e14a6
陈兴旺 <flatychen@126.com>
2018-01-23 15:11:59 +0800

    完善日志

--- src/main/java/com/company/project/web/exception/DefalutExceptionHandler.java
+++ src/main/java/com/company/project/web/exception/DefalutExceptionHandler.java
@@ -4,10 +4,13 @@ import com.company.project.base.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
  * spring mvc 全局异常处理
@@ -21,12 +24,55 @@ public class DefalutExceptionHandler {
     private static Logger log = LoggerFactory
             .getLogger(DefalutExceptionHandler.class);
 
+    /**
+     * 添加404
+     *
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Response<? extends Object> notFound(
+            Exception ex) {
+        return Response.fail("404 not found");
+    }
+
+
+    /**
+     * 统一异常处理
+     *
+     * @param ex
+     * @return
+     */
     @ExceptionHandler(Throwable.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Response<? extends Object> handleException(
             Exception ex) {
+        if (ex instanceof BindException) {
+            BindException e = (BindException) ex;
+            log.warn("BindException:{}", ex.getMessage());
+            return Response.fail(e.getMessage());
+        }
+        if (ex instanceof MissingServletRequestParameterException) {
+            MissingServletRequestParameterException e = (MissingServletRequestParameterException) ex;
+            log.warn("MissingServletRequestParameterException:{}", ex.getMessage());
+            return Response.fail(e.getMessage());
+        }
         log.error("Exception", ex);
         return Response.serverError();
+    }
+
+    /**
+     * 统一异常处理
+     *
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(value = {BindException.class, MissingServletRequestParameterException.class})
+    public Response<? extends Object> requestNotValid(
+            Exception ex) {
+        log.error("BindException:{}", ex.getMessage());
+        return Response.fail(ex.getMessage());
     }
 
 
