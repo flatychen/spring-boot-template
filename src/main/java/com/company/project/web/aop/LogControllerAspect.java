8bb9d7b67fedb95f1435d7db2aa43594639b23c2
flatychen <flatychen@qq.com>
2018-04-25 10:29:35 +0800

    完善日志

---
+++ src/main/java/com/company/project/web/aop/LogControllerAspect.java
@@ -0,0 +1,67 @@
+package com.company.project.web.aop;
+
+import com.alibaba.fastjson.JSON;
+import com.company.project.utils.web.WebUtil;
+import com.google.common.base.Stopwatch;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
+@Aspect
+@Component
+public class LogControllerAspect {
+
+
+    private static final Logger log = LoggerFactory
+            .getLogger(LogControllerAspect.class);
+
+    @Value("${sms.ipwhitelist:''}")
+    private String ipwhiteList;
+
+
+    @Autowired(required = false)
+    private HttpServletRequest request;
+
+    @Around("execution(* com.company.project.web.controller..*(..))")
+    public Object requestPointCut(ProceedingJoinPoint pjp)
+            throws Throwable {
+        String requestIp = WebUtil.getRealIpAddr(request);
+        logRequestLog(requestIp);
+        Stopwatch watch = Stopwatch.createStarted();
+        Object result = pjp.proceed();
+        watch.stop();
+        log.info("[{}:{}],response <= {},time:{}", requestIp, request.getServletPath(),
+                result, watch.toString());
+        return result;
+    }
+
+
+    @Around("execution(* com.company.project.web.exception..*(..)) ")
+    public Object exceptionPointCut(ProceedingJoinPoint pjp)
+            throws Throwable {
+        Object result = pjp.proceed();
+        log.error("error [{}:{}],request:{},response <= {} ", WebUtil.getRealIpAddr(request), request.getServletPath
+                (), JSON.toJSONString
+                (request.getParameterMap()), result);
+        return result;
+    }
+
+
+    private void logRequestLog(String requestIp) {
+        MDC.put("traceId", UUID.randomUUID().toString());
+        log.info("[{}:{}],request  => {}", requestIp, request.getServletPath(), JSON.toJSONString
+                (request.getParameterMap()));
+    }
+
+
+
+}
