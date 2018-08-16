0fc3012a06b5aaade7f715776e9f04d5dad568f3
flatychen <flatychen@126.com>
2018-08-16 10:09:30 +0800

    完善包结构

--- src/main/java/com/company/project/utils/Util.java
+++ src/main/java/com/company/project/utils/Util.java
@@ -2,8 +2,34 @@ package com.company.project.utils;
 
 import lombok.extern.slf4j.Slf4j;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 public class Util {
 
+
+    private static final String[] PROXY_REMOTE_IP_ADDRESS = {
+            "X-Forwarded-For", "X-Real-IP", "WL-Proxy-Client-IP",
+            "HTTP_CLIENT_IP", "HTTP_CLIENT_IP"};
+
+    /**
+     * 正式IP源获取
+     *
+     * @param request
+     * @return
+     */
+    public static String getRealIpAddr(HttpServletRequest request) {
+        for (int i = 0; i < PROXY_REMOTE_IP_ADDRESS.length; i++) {
+            String ips = request.getHeader(PROXY_REMOTE_IP_ADDRESS[i]);
+            if (ips != null && ips.trim().length() > 0) {
+                int commaOffset = ips.indexOf(',');
+                if (commaOffset < 0) {
+                    return ips;
+                }
+                return ips.substring(0, commaOffset);
+            }
+        }
+        return request.getRemoteHost();
+    }
 
 }
