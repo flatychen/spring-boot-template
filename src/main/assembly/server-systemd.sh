8298eb90cc22edbd11081f17e3f1a846be143f8d
flatychen <flatychen@126.com>
2018-12-20 15:37:55 +0800

    完善shell

--- src/main/assembly/server-systemd.sh
+++ src/main/assembly/server-systemd.sh
@@ -2,7 +2,9 @@ name="litemall"
 
 dir=`pwd`
 jarname=`ls | grep -e "^\${name}.*jar$" | sort -r | head -n 1`
+chmod +x ${jarName}
 dir=`echo ${dir//\//\\\/}`
-sed -i "s/^ExecStart.*/ExecStart=${dir}${jarname}/g" /etc/systemd/system/${name}.service
-systemctrl restart ${name}.service
-systemctrl status ${name}.service
+sed -i "s/^ExecStart.*/ExecStart=${dir}\/${jarname}/g" /etc/systemd/system/${name}.service
+systemctl daemon-reload
+systemctl restart ${name}.service
+systemctl status ${name}.service
