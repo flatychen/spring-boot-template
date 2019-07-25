5ad144a472785a062e13b549ccf0a91359d14ddb
flatychen <flatychen@126.com>
2019-07-25 10:59:22 +0800

    添加表前缀支持

--- src/test/java/CodeGen/CodeGenerator.java
+++ src/test/java/CodeGen/CodeGenerator.java
@@ -18,18 +18,19 @@ import java.util.*;
  */
 public class CodeGenerator {
     //JDBC配置，请修改为你项目的实际配置
-    private static final String JDBC_URL = "jdbc:mysql://mysql.m.aomygod.api:3306/litemall";
+    private static final String JDBC_URL = "jdbc:mysql://mysql.m.aomygod.api:3306/admin_user_center";
     private static final String JDBC_USERNAME = "dev";
     private static final String JDBC_PASSWORD = "123456";
     private static final String JDBC_DIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
-    public static final String BASE_PACKAGE = "com.company.project";//项目基础包名称，根据自己公司的项目修改
+    public static final String BASE_PACKAGE = "cn.aylives.admin.user.center";//项目基础包名称，根据自己公司的项目修改
+    public static final String TABLE_PREFIX = "t_";//表前缀
 
 
     public static void main(String[] args) {
 //        Gen.getInstance().genCode("lucky_bag");
 //        Gen.getInstance().genCode("litemall_goods","litemall_goods_product");
-        Gen.getInstance().genCode("litemall_brand","litemall_category");
-//        Gen.getInstance().genCode("bbb", "xxx");
+//        Gen.getInstance().genCode("litemall_brand","litemall_category");
+        Gen.getInstance().genCode("t_dept");
 //        Gen.getInstance().genCodeByCustomModelName("输入表名", "输入自定义Model名称");
     }
 
@@ -91,6 +92,12 @@ public class CodeGenerator {
          * @param modelName 自定义的 Model 名称
          */
         public static void genCodeByCustomModelName(String tableName, String modelName) {
+
+            // 计算前缀
+            if (StringUtils.isNotEmpty(TABLE_PREFIX)) {
+                modelName = StringUtils.substringAfter(tableName, TABLE_PREFIX);
+            }
+
             genModelAndMapper(tableName, modelName);
             genService(tableName, modelName);
             genController(tableName, modelName);
@@ -135,6 +142,7 @@ public class CodeGenerator {
             TableConfiguration tableConfiguration = new TableConfiguration(context);
             tableConfiguration.setTableName(tableName);
             if (StringUtils.isNotEmpty(modelName)) tableConfiguration.setDomainObjectName(modelName);
+
             tableConfiguration.setGeneratedKey(new GeneratedKey("id", "Mysql", true, null));
             context.addTableConfiguration(tableConfiguration);
 
@@ -177,21 +185,21 @@ public class CodeGenerator {
                 data.put("basePackage", BASE_PACKAGE);
 
                 File file = new File(PROJECT_PATH + JAVA_PATH + PACKAGE_PATH_SERVICE + modelNameUpperCamel + "Service" +
-                        ".java");
+                                             ".java");
                 if (!file.getParentFile().exists()) {
                     file.getParentFile().mkdirs();
                 }
                 cfg.getTemplate("service.ftl").process(data,
-                        new FileWriter(file));
+                                                       new FileWriter(file));
                 System.out.println(modelNameUpperCamel + "Service.java 生成成功");
 
                 File file1 = new File(PROJECT_PATH + JAVA_PATH + PACKAGE_PATH_SERVICE_IMPL + modelNameUpperCamel +
-                        "ServiceImpl.java");
+                                              "ServiceImpl.java");
                 if (!file1.getParentFile().exists()) {
                     file1.getParentFile().mkdirs();
                 }
                 cfg.getTemplate("service-impl.ftl").process(data,
-                        new FileWriter(file1));
+                                                            new FileWriter(file1));
                 System.out.println(modelNameUpperCamel + "ServiceImpl.java 生成成功");
             } catch (Exception e) {
                 throw new RuntimeException("生成Service失败", e);
@@ -213,7 +221,7 @@ public class CodeGenerator {
                 data.put("basePackage", BASE_PACKAGE);
 
                 File file = new File(PROJECT_PATH + JAVA_PATH + PACKAGE_PATH_CONTROLLER + modelNameUpperCamel +
-                        "Controller.java");
+                                             "Controller.java");
                 if (!file.getParentFile().exists()) {
                     file.getParentFile().mkdirs();
                 }
@@ -229,7 +237,7 @@ public class CodeGenerator {
 
         private static freemarker.template.Configuration getConfiguration() throws IOException {
             freemarker.template.Configuration cfg = new freemarker.template.Configuration(freemarker.template
-                    .Configuration.VERSION_2_3_23);
+                                                                                                  .Configuration.VERSION_2_3_23);
             cfg.setDirectoryForTemplateLoading(new File(TEMPLATE_FILE_PATH));
             cfg.setDefaultEncoding("UTF-8");
             cfg.setTemplateExceptionHandler(TemplateExceptionHandler.IGNORE_HANDLER);
