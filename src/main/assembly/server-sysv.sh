796b353c5792f4d03724988bc864dff5594a7505
xingwang.chen <xingwang.chen@ericsson.com>
2020-12-11 10:38:00 +0800

    去除得杂shell

--- src/main/assembly/server-sysv.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-name="litemall"
-jarName=`ls | grep -e "^\${name}.*jar$" | sort -r | head -n 1`
-chmod +x ${jarName}
-echo `pwd`/${jarName}
-ln -f -s `pwd`/${jarName}  /etc/init.d/${name}
-service litemall restart
