06a8946ef66162f734775ddc7fa603a17aabfd5d
flatychen <flatychen@126.com>
2020-12-16 21:38:52 +0800

    merge

--- src/main/java/com/company/project/Application.java
+++ src/main/java/com/company/project/Application.java
@@ -7,11 +7,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 @MapperScan("com.company.project.dao")
 public class Application {
-
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println("start success");
     }
 }
 
